3d1bb8d6a830ac462d5f8dc2c07333fe5a0bedd5
Cheyenne <chzuck@my365.bellevue.edu>
2023-04-16 18:39:35 -0400

    Week 5 Attempt 1

---
+++ Week 5/bookclub/src/main/java/com/bookclub/model/WishlistItem.java
@@ -0,0 +1,60 @@
+package com.bookclub.model;
+
+
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class WishlistItem {
+    @NotNull
+    @NotEmpty(message="ISBN is a required field.")
+    private String isbn;
+    @NotNull
+    @NotEmpty(message="Title is a required field.")
+    private String title;
+    @Id
+    private String id;
+
+    public WishlistItem(){
+
+    }
+
+    public WishlistItem(String isbn, String title){
+        setIsbn(isbn);
+        setTitle(title);
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setIsbn(String newIsbn){
+        this.isbn = newIsbn;
+    }
+
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+
+    public void setId(String newId){
+        this.id = newId;
+    }
+
+
+    @Override
+    public String toString(){
+        String template = "WishlistItem{id=%s, isbn=%s, title=%s}";
+        String output = String.format(template, getId(), getIsbn(), getTitle().toString());
+        return output;
+    }
+}
---
+++ Week 5/bookclub/src/main/java/com/bookclub/service/impl/MemBookDao.java
@@ -0,0 +1,43 @@
+package com.bookclub.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.dao.BookDao;
+
+public class MemBookDao implements BookDao{
+    @Autowired
+    private List<Book> books;
+
+    public MemBookDao(){
+        this.books = new ArrayList<Book>();
+        Book book1 = new Book("978-0062060624", "The Song of Achilles", "A tale of gods, kings, immortal fame, and the human heart.", 416, new ArrayList<>(List.of("Madeline Miller")));
+        Book book2 = new Book("9780593593806", "Spare", "A memoir by Prince Harry, Duke of Sussex, which exposes his enemies.", 416, new ArrayList<>(List.of("Prince Harry, Duke of Sussex")));
+        Book book3 = new Book("978-0-307-59331-3", "1Q84", "The fates of 2 people are intertwined in Tokyo.", 928, new ArrayList<>(List.of("Haruki Murakami")));
+        Book book4 = new Book("978-1-524-79628-0", "Fire and Blood", "A prequel to the 'A Song of Ice and Fire' series, illuminating the past of the Targaryens.", 736, new ArrayList<>(List.of("George R.R. Martin")));
+        Book book5 = new Book("9780702300172", "A Ballad of Songbirds and Snakes", "A prequel to the 'Hunger Games' trilogy, looking at the rise of President Snow.", 517, new ArrayList<>(List.of("Suzanne Collins")));
+        this.books.add(book1);
+        this.books.add(book2);
+        this.books.add(book3);
+        this.books.add(book4);
+        this.books.add(book5);
+    }
+
+    @Override
+    public List<Book> list(){
+        return this.books;
+    }
+
+    @Override
+    public Book find(String key) {
+        for (Book book : this.books) {
+            if (book.getIsbn().equals(key)) {
+                return book;
+            }
+        }
+        return new Book();
+    }
+}
